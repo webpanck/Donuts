f8225a6c1f51900f996be214a36ff11dd405b9e3
潘诚宽 <chengkuan0327@163.com>
2021-05-24 11:21:36 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
-  <button class="d-button" :class="{[`icon-${iconPosition}`]: true}">
-    <d-icon class="icon" v-if="icon" :name="icon"></d-icon>
-    <d-icon class="loading" name="loading"></d-icon>
+  <button class="d-button" :class="{[`icon-${iconPosition}`]: true}"
+    @click="$emit('click')">
+    <d-icon class="icon" v-if="icon && !loading" :name="icon"></d-icon>
+    <d-icon class="loading icon" v-if="loading" name="loading"></d-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -13,6 +14,10 @@
     //props: ['icon', 'iconPosition']
     props: {
       icon: {},
+      loading: {
+        type: Boolean,
+        default: false
+      },
       iconPosition: {
         type: String,
         default: 'left',
