1411170864c8d756d9cac5cc68017d460fe40f02
潘诚宽 <chengkuan0327@163.com>
2021-05-24 11:53:35 +0800

    创建 button group

---
+++ src/button-group.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .d-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .d-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
