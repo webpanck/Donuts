a3c890764da931858440a0c898af039335debf95
潘诚宽 <chengkuan0327@163.com>
2021-07-14 20:47:39 +0800

    添加 tabs 相关组件，添加基本 props

--- src/app.js
+++ src/app.js
@@ -12,6 +12,11 @@ import Content from './content'
 import Footer from './footer'
 import Toast from './toast'
 import plugin from './plugin';
+import Tabs from './tabs'
+import TabsHead from './tabs-head'
+import TabsItem from './tabs-item'
+import TabsBody from './tabs-body'
+import TabsPane from './tabs-pane'
 
 Vue.component('d-button', Button)
 Vue.component('d-icon', Icon)
@@ -26,14 +31,16 @@ Vue.component('d-content', Content)
 Vue.component('d-footer', Footer)
 Vue.component('d-toast', Toast)
 Vue.use(plugin)
+Vue.component('d-tabs', Tabs)
+Vue.component('d-tabs-head', TabsHead)
+Vue.component('d-tabs-item', TabsItem)
+Vue.component('d-tabs-body', TabsBody)
+Vue.component('d-tabs-pane', TabsPane)
 
 new Vue({
     el: '#app',
     data: {
-        loading1: false,
-        loading2: false,
-        loading3: false,
-        message: '大潘'
+        selectedTab: 'sports'
     },
     created() {
     },
