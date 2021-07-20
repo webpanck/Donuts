cfeb4f9226c98b5a907113c69240b0db0df6b8e8
潘诚宽 <chengkuan0327@163.com>
2021-07-20 09:44:56 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,21 +31,29 @@
         eventBus: this.eventBus
       }
     },
-    mounted() {
-      if(this.$children.length === 0) {
-        console && console.warn &&
-        console.warn('tabs 的子组件只能是 tabs-head 和 tabs-bod，但你没有写子组件')
-      }
-      this.$children.forEach((vm) => {
-        if(vm.$options.name === 'DonutsTabsHead') {
-          vm.$children.forEach((childVm) => {
-            if(childVm.$options.name === 'DonutsTabsItem'
-              && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren() {
+        if(this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs 的子组件只能是 tabs-head 和 tabs-bod，但你没有写子组件')
         }
-      })
+      },
+      selectTab() {
+        this.$children.forEach((vm) => {
+          if(vm.$options.name === 'DonutsTabsHead') {
+            vm.$children.forEach((childVm) => {
+              if(childVm.$options.name === 'DonutsTabsItem'
+                  && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted() {
+      this.checkChildren()
+      this.selectTab()
     }
   }
 </script>
