aeba2c84358f782855cbe608f6b1888b15f97d91
潘诚宽 <chengkuan0327@163.com>
2021-07-21 13:26:54 +0800

    创建官网

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <d-button>默认按钮</d-button>
+    <d-button icon="settings">默认按钮</d-button>
+    <d-button :loading="true">默认按钮</d-button>
+    <d-button disabled>默认按钮</d-button>
+
+    <pre>
+      <code>
+        {{content}}
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+  import Button from '../../../src/button'
+  export default {
+    components: {
+      'd-button': Button
+    },
+    data() {
+      return {
+        content: `
+          <d-button>默认按钮</d-button>
+          <d-button icon="settings">默认按钮</d-button>
+          <d-button :loading="true">默认按钮</d-button>
+          <d-button disabled>默认按钮</d-button>
+        `
+      }
+    }
+  }
+</script>
