f608652559abcc458515707f7b67aa41b30c8600
潘诚宽 <chengkuan0327@163.com>
2021-07-21 19:36:51 +0800

    完成官网 grid 组件的展示

---
+++ docs/.vuepress/components/grid-demo-3.vue
@@ -0,0 +1,154 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>设置空隙</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <d-row class="demoRow" gutter="10">
+      <d-col span="8">
+        <div class="demoCol">8</div>
+      </d-col>
+      <d-col span="8" offset="8">
+        <div class="demoCol">8</div>
+      </d-col>
+    </d-row>
+    <d-row class="demoRow" gutter="10">
+      <d-col span="6" offset="6">
+        <div class="demoCol">6</div>
+      </d-col>
+      <d-col span="6" offset="6">
+        <div class="demoCol">6</div>
+      </d-col>
+    </d-row>
+    <d-row class="demoRow" gutter="10">
+      <d-col span="4">
+        <div class="demoCol">4</div>
+      </d-col>
+      <d-col span="4" offset="4">
+        <div class="demoCol">4</div>
+      </d-col>
+      <d-col span="4" offset="8">
+        <div class="demoCol">4</div>
+      </d-col>
+    </d-row>
+    <d-row class="demoRow" gutter="10">
+      <d-col span="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2" offset="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2" offset="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2" offset="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2">
+        <div class="demoCol">2</div>
+      </d-col>
+      <d-col span="2" offset="2">
+        <div class="demoCol">2</div>
+      </d-col>
+    </d-row>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.demoRow{
+  margin: 10px 0;
+}
+.demoCol {
+  height: 50px;
+  border: 1px solid #ccc;
+  background: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+<script>
+import Row from '../../../src/row'
+import Col from '../../../src/col'
+export default {
+  components: {
+    'd-row': Row,
+    'd-col': Col
+  },
+  data () {
+    return {
+      content: `
+          * {
+            box-sizing: border-box;
+          }
+          <d-row class="demoRow" gutter="10">
+            <d-col span="8">
+              <div class="demoCol">8</div>
+            </d-col>
+            <d-col span="8" offset="8">
+              <div class="demoCol">8</div>
+            </d-col>
+          </d-row>
+          <d-row class="demoRow" gutter="10">
+            <d-col span="6" offset="6">
+              <div class="demoCol">6</div>
+            </d-col>
+            <d-col span="6" offset="6">
+              <div class="demoCol">6</div>
+            </d-col>
+          </d-row>
+          <d-row class="demoRow" gutter="10">
+            <d-col span="4">
+              <div class="demoCol">4</div>
+            </d-col>
+            <d-col span="4" offset="4">
+              <div class="demoCol">4</div>
+            </d-col>
+            <d-col span="4" offset="8">
+              <div class="demoCol">4</div>
+            </d-col>
+          </d-row>
+          <d-row class="demoRow" gutter="10">
+            <d-col span="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2" offset="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2" offset="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2" offset="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2">
+              <div class="demoCol">2</div>
+            </d-col>
+            <d-col span="2" offset="2">
+              <div class="demoCol">2</div>
+            </d-col>
+          </d-row>
+      `.replace(/^ {8}/gm, '').trim()
+    }
+  }
+}
+</script>
