8b031fb7ff60acc7b1a403561a246530155e1f32
潘诚宽 <chengkuan0327@163.com>
2021-07-21 19:35:41 +0800

    完成官网 button 组件的展示

--- docs/.vuepress/components/button-demo-1.vue
+++ docs/.vuepress/components/button-demo-1.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
+  <div style="padding-top: 16px">
+    <h2>简单用法</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
     <d-button>默认按钮</d-button>
     <d-button icon="settings">默认按钮</d-button>
     <d-button :loading="true">默认按钮</d-button>
     <d-button disabled>默认按钮</d-button>
 
+    <p>
+      <strong>代码</strong>
+    </p>
     <pre><code>{{content}}</code></pre>
   </div>
 </template>
